af87065e9efed44cc65c8df1c6f17c247af6a03f
arfin-arif <arifmtbd@gmail.com>
2022-08-22 23:59:28 +0600

    added calculaing total expense

--- js/calculation.js
+++ js/calculation.js
@@ -1,12 +1,24 @@
+function playerExpenses() {
+    const perPlayerCost = getInputFiledValueById('per-player-cost')
+    const playerExpense = perPlayerCost * cartArray.length;
+    const playerExpenseInitialValue = document.getElementById('player-expense');
+    const playerTotalExpense = playerExpenseInitialValue.innerText = playerExpense;
+    return playerTotalExpense;
+
+}
+
 
 document.getElementById('calculate').addEventListener('click', function () {
-    const perPlayerCost = getInputFiledValueById('per-player-cost')
-    console.log(perPlayerCost)
+    const playerTotalCost = playerExpenses();
 
 });
 document.getElementById('calculate-total').addEventListener('click', function () {
     const managerCost = getInputFiledValueById('manager-cost');
     const coachCost = getInputFiledValueById('coach-cost');
-    console.log(coachCost);
+    const playerTotalCost = playerExpenses();
+    const totalCost = managerCost + coachCost + playerTotalCost;
+    const totalExpenseInitial = document.getElementById('total-expense');
+    totalExpenseInitial.innerText = totalCost;
+    console.log(totalCost);
     console.log(managerCost);
 })
